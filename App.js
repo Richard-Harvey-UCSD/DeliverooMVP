eafe28696a287264ba97d36c2a3bd5b86da6edb5
Richard Harvey <theerichardharvey@gmail.com>
2022-08-26 12:11:14 -0400

    Updated HomeScreen

--- App.js
+++ App.js
@@ -7,16 +7,11 @@
  */
 
 import React from 'react';
-import {
-  StatusBar,
-  Text,
-  View,
-} from 'react-native';
 
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './Screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 
 
 const Stack = createNativeStackNavigator();
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, SafeAreaView, Text, Image } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  VerticalAdjustmentIcon,
+} from 'react-native-heroicons/outline';
+
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text className='text-red-500'>HomeScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className='bg-white pt-5'>
+      <Text className='text-red-500'>
+
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru'
+            }}
+            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+          />
+
+          <View className='flex-1'>
+            <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+            <Text className='font-bold text-xl'>
+              Current Location
+              <ChevronDownIcon size={20} color='#00CCBB' />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color='#00CCBB' />
+        </View>
+
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
